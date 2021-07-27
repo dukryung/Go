47cbd57f71e91f9d5889ed222bd31f461c6c50db
dukryung <dukryung@gmail.com>
2021-07-27 23:07:58 +0900

    [programmers] - [coding test] Delivery  Questions commit

---
+++ Projects/CodingTest/Programmers/Delivery/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+}
+
+func solution(N int, road [][]int, k int) int {
+	answer := 0
+
+	// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+	fmt.Printf("Hello Go")
+
+	return answer
+}
