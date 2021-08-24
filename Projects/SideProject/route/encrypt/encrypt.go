3f7de75143100ed4ad46426b23ddc023e3692963
dukryung <dukryung@gmail.com>
2021-08-24 23:03:27 +0900

    [side-project] encrypt logic commit

--- Projects/SideProject/route/encrypt/encrypt.go
+++ Projects/SideProject/route/encrypt/encrypt.go
@@ -6,16 +6,40 @@ import (
 	"fmt"
 )
 
-func Encrypt(text string) (string, error) {
+func GenKey() (*rsa.PrivateKey, error) {
 	keys, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
 		fmt.Println("[ERR] failed to generate encrypt keys")
+		return nil, err
+	}
+
+	return keys, nil
+}
+
+func Encrypt(text string, keys *rsa.PrivateKey) (string, error) {
+	pk := &keys.PublicKey
+	ciphertext, err := rsa.EncryptPKCS1v15(rand.Reader,
+		pk,
+		[]byte(text))
+
+	if err != nil {
+		fmt.Println("[ERR] failed to encrypt text")
 		return "", nil
 	}
 
-	publickey := keys.PublicKey
+	return string(ciphertext), nil
 }
 
-func Decrypt(text string) (string, error) {
+func Decrypt(ciphertext string, privtekey *rsa.PrivateKey) (string, error) {
+	plaintext, err := rsa.DecryptPKCS1v15(
+		rand.Reader,
+		privtekey,
+		[]byte(ciphertext),
+	)
+	if err != nil {
+		fmt.Println("[ERR] failed to decrypte text")
+		return "", err
+	}
 
+	return string(plaintext), nil
 }
