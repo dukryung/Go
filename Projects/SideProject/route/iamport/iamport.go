9b9e95235438579368d7b5561758768077bf05be
dukryung <dukryung@gmail.com>
2021-08-25 00:18:04 +0900

    [side-project] add compare price function in iamport.go commit

--- Projects/SideProject/route/iamport/iamport.go
+++ Projects/SideProject/route/iamport/iamport.go
@@ -1,26 +1,21 @@
 package iamport
 
 import (
+	"bytes"
 	"database/sql"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
 	"net/http"
-	"net/url"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gofrs/uuid"
 )
 
-// Client need for networking with iamport's server.
-type Client struct {
-	APIKey      string
-	APISecret   string
-	AccessToken accessToken
-	HTTP        *http.Client
-}
+var APIKey = "5159389833177379"
+var APISecret = "GkmriPyRX9Qf0O8m7i4TGOThaYgDBPpi9MKsCikzsaPtZXQ7gU1XJUeTTvwukWMuIwWkPgUirJLrGByX"
 
 type accessToken struct {
 	Token   string
@@ -28,7 +23,11 @@ type accessToken struct {
 }
 
 type Iamport struct {
-	DB *sql.DB
+	DB          *sql.DB
+	APIKey      string
+	APISecret   string
+	AccessToken accessToken
+	HTTP        *http.Client
 }
 
 // Payment information
@@ -72,78 +71,13 @@ type REQRSC struct {
 }
 
 func (iam *Iamport) Routes(route *gin.RouterGroup) {
-	route.GET("/payment/information", iam.getPAYMT)
-	route.POST("/payment/complete", iam.checkPAYMT)
-}
-
-//PAYT is abbreviation of payment.
-//checkPAYMT checks the payment information at iamport.
-func (iam *Iamport) checkPAYMT(c *gin.Context) {
-	reqrsc := &REQRSC{}
-	err := c.ShouldBindJSON(reqrsc)
-	if err != nil {
-		log.Println("[ERR] failed to extract request resourece err : ", err)
-		c.JSON(http.StatusBadRequest, nil)
-		return
-	}
-}
-
-//IMPT is abbreviation of iamport.
-//GetIMPTToken get authentication token from iamport.
-func (cli *Client) GetIMPTToken() error {
-	if cli.APIKey == "" {
-		return errors.New("iamport: APIKey is missing")
-	}
-
-	if cli.APISecret == "" {
-		return errors.New("iamport: APISecret is missing")
-	}
-
-	form := url.Values{}
-	form.Set("imp_key", cli.APIKey)
-	form.Set("imp_secret", cli.APISecret)
-
-	res, err := cli.HTTP.PostForm("https://api.iamport.kr/users/getToken", form)
-	if err != nil {
-		return err
-	}
-
-	if res.StatusCode == http.StatusUnauthorized {
-		return errors.New("iamport: unauthorized")
-	}
-
-	if res.StatusCode != http.StatusOK {
-		return errors.New("iamport: unknown error")
-	}
-
-	data := struct {
-		Code     int    `json:"code"`
-		Message  string `json:"message"`
-		Response struct {
-			AccessToken string `json:"access_token"`
-			ExpiredAt   int64  `json:"expired_at"`
-			Now         int64  `json:"now"`
-		} `json:"response"`
-	}{}
-
-	err = json.NewDecoder(res.Body).Decode(&data)
-	if err != nil {
-		return err
-	}
-
-	if data.Code != 0 {
-		return fmt.Errorf("iamport: %s", data.Message)
-	}
-
-	cli.AccessToken.Token = data.Response.AccessToken
-	cli.AccessToken.Expired = time.Unix(data.Response.ExpiredAt, 0)
-
-	return nil
+	route.GET("/payment/information", iam.GetPAYMT)
+	route.POST("/payment/complete", iam.CheckPAYMT)
 }
 
 //PAYT is abbreviation of payment.
 //getPAYT reads information required for payment request.
-func (iam *Iamport) getPAYMT(c *gin.Context) {
+func (iam *Iamport) GetPAYMT(c *gin.Context) {
 	reqrsc := &REQRSC{}
 	err := c.ShouldBindJSON(reqrsc)
 	if err != nil {
@@ -179,6 +113,241 @@ func (iam *Iamport) getPAYMT(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, p)
+}
+
+//PAYT is abbreviation of payment.
+//checkPAYMT checks the payment information at iamport.
+func (iam *Iamport) CheckPAYMT(c *gin.Context) {
+	reqrsc := &REQRSC{}
+	err := c.ShouldBindJSON(reqrsc)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, nil)
+		return
+	}
+
+	fmt.Println("request resource : ", reqrsc)
+
+	err = iam.GetIMPTToken()
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, nil)
+		return
+	}
+
+	pay, err := iam.GetPAYMTImpUID(reqrsc.Iid)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, nil)
+		return
+	}
+
+	fmt.Println("pay:", pay)
+
+	p, err := iam.GetPrice(reqrsc.Pid)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, nil)
+		return
+	}
+
+	err = iam.ComparePrice(pay.Amount, p)
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusBadRequest, nil)
+		return
+	}
+
+	//{----TODO : save payment information to DB.
+	//-----}
+
+	c.JSON(http.StatusOK, nil)
+}
+func (iam *Iamport) SavePAYMTTODB(pay PAYMT) {
+	//{----TODO : save  buy history information.
+	//-----}
+
+	//{----TODO : save  sell history information.
+	//-----}
+
+	//{----TODO : insert buy history at user's buy history (maybe should make table)
+	//-----}
+
+	//{----TODO : update sell count at user table
+	//-----}
+}
+
+func (iam *Iamport) ComparePrice(amount int64, price int64) error {
+
+	if price != amount {
+		return errors.New("[ERR] not same price ")
+	}
+
+	return nil
+}
+func (iam *Iamport) GetPrice(projectid int64) (int64, error) {
+	stmt, err := iam.DB.Prepare(`SELECT price FROM project WHERE id = ?`)
+	if err != nil {
+
+		return 0, errors.New("[ERR] failed to prepare statement project price query")
+	}
+
+	defer stmt.Close()
+
+	rows, err := stmt.Query(projectid)
+	if err != nil {
+		return 0, errors.New("[ERR] failed to query err")
+	}
+
+	var price int64
+	for rows.Next() {
+		err = rows.Scan(price)
+		if err != nil {
+			return 0, errors.New("[ERR] failed to query err")
+		}
+	}
+
+	return price, nil
+}
+
+//IMPT is abbreviation of iamport.
+//GetIMPTToken get authentication token from iamport.
+func (iam *Iamport) GetIMPTToken() error {
+	reqsrc := struct {
+		ImpKey    string `json:"imp_key"`
+		ImpSecret string `json:"imp_secret"`
+	}{}
+
+	reqsrc.ImpKey = iam.APIKey
+	reqsrc.ImpSecret = iam.APISecret
+
+	if iam.APIKey == "" {
+		return errors.New("[ERR] APIKey is missing")
+	}
+
+	if iam.APISecret == "" {
+		return errors.New("[ERR] APISecret is missing")
+	}
+
+	imdata, err := json.Marshal(reqsrc)
+	if err != nil {
+		return errors.New("[ERR] failed to json marshal")
+	}
+
+	buf := bytes.NewBuffer(imdata)
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", "https://api.iamport.kr/users/getToken", buf)
+	if err != nil {
+		return errors.New("[ERR] failed to json marshal")
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		return errors.New("[ERR] client do err")
+	}
+
+	if res.StatusCode == http.StatusUnauthorized {
+		return errors.New("[ERR] unauthorized")
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return errors.New("[ERR] unknown error")
+	}
+
+	data := struct {
+		Code     int    `json:"code"`
+		Message  string `json:"message"`
+		Response struct {
+			AccessToken string `json:"access_token"`
+			ExpiredAt   int64  `json:"expired_at"`
+			Now         int64  `json:"now"`
+		} `json:"response"`
+	}{}
+
+	err = json.NewDecoder(res.Body).Decode(&data)
+	if err != nil {
+		return err
+	}
+
+	if data.Code != 0 {
+		return fmt.Errorf("[ERR] %s", data.Message)
+	}
+
+	iam.AccessToken.Token = data.Response.AccessToken
+	iam.AccessToken.Expired = time.Unix(data.Response.ExpiredAt, 0)
+
+	return nil
+}
+
+func (iam *Iamport) authorization() (string, error) {
+	now := time.Now()
+
+	switch {
+	case iam.AccessToken.Token == "",
+		iam.AccessToken.Expired.IsZero(),
+		!iam.AccessToken.Expired.Before(now):
+
+		err := iam.GetIMPTToken()
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return iam.AccessToken.Token, nil
+}
+
+// GetPAYMTImpUID reads  payment information using iamport number.
+// GET /payments/{imp_uid}
+func (iam *Iamport) GetPAYMTImpUID(impuid string) (PAYMT, error) {
+	data := struct {
+		Code     int    `json:"code"`
+		Message  string `json:"message"`
+		Response PAYMT  `json:"response"`
+	}{}
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://api.iamport.kr/payments/%s", impuid), nil)
+	if err != nil {
+		return data.Response, err
+	}
+
+	auth, err := iam.authorization()
+	if err != nil {
+		return data.Response, err
+	}
+
+	req.Header.Set("Authorization", auth)
+
+	res, err := client.Do(req)
+	if err != nil {
+		return data.Response, err
+	}
+
+	if res.StatusCode == http.StatusUnauthorized {
+		return data.Response, errors.New("[ERR] unauthorized")
+	}
+
+	if res.StatusCode == http.StatusNotFound {
+		return data.Response, errors.New("[ERR] invalid imp_uid")
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return data.Response, errors.New("[ERR] unknown error")
+	}
+
+	err = json.NewDecoder(res.Body).Decode(&data)
+	if err != nil {
+		return data.Response, err
+	}
+
+	if data.Code != 0 {
+		return data.Response, fmt.Errorf("[ERR] %s", data.Message)
+	}
+
+	return data.Response, nil
 }
 
 //PAYMTRSC is abbreviation of payment resource.
